4ff8f9977eca976cd725622a5d4cf1b956dbd389
Kledjo Malaj <kledjomalaj20@gmail.com>
2025-09-03 23:56:51 +0200

    HomePage frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+function App() {
+
+  return (
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+          </Routes>
+        </div>
+      </Router>
+  )
+}
+
+export default App
